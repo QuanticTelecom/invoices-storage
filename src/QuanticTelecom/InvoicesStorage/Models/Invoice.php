1b59b2622bf41284c615446b32f31b8924bee05b
Thibaud <thibaud@dauce.fr>
2015-03-06 13:27:11 +0100

    feat: create Invoice model
    Require "quantic-telecom/storage" package
    Create Invoice.php file

---
+++ src/QuanticTelecom/InvoicesStorage/Models/Invoice.php
@@ -0,0 +1,13 @@
+<?php namespace QuanticTelecom\InvoicesStorage\Models;
+
+use QuanticTelecom\Storage\Model;
+
+class Invoice extends Model
+{
+    /**
+     * The collection associated with the model.
+     *
+     * @var string
+     */
+    protected $collection = "invoices";
+}
