75d0d29251f76ad6b6ab1feca97492c8eacfc12b
Thibaud <thibaud@dauce.fr>
2015-03-06 14:10:53 +0100

    feat: add CustomerTranslatorInterface contract
    Create CustomerTranslatorInterface with two methods to switch between the model and the business logic

---
+++ src/QuanticTelecom/InvoicesStorage/Contracts/CustomerTranslatorInterface.php
@@ -0,0 +1,23 @@
+<?php namespace QuanticTelecom\InvoicesStorage\Contracts;
+
+use QuanticTelecom\Invoices\Contracts\CustomerInterface as CustomerLogic;
+use QuanticTelecom\InvoicesStorage\Models\Customer as CustomerModel;
+
+interface CustomerTranslatorInterface
+{
+    /**
+     * @param CustomerModel $customerModel
+     * @param CustomerLogic $customerLogic | null Customer logic instance to use
+     * @return CustomerLogic
+     */
+    public function customerModelToCustomerLogic(
+        CustomerModel $customerModel,
+        CustomerLogic $customerLogic = null
+    );
+
+    /**
+     * @param CustomerLogic $customerLogic
+     * @return CustomerModel
+     */
+    public function customerLogicToCustomerModel(CustomerLogic $customerLogic);
+}
