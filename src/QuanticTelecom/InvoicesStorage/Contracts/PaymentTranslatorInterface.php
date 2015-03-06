d1c5de595fe2d21ccb438457230ad7affdeb9c8d
Thibaud <thibaud@dauce.fr>
2015-03-06 14:22:49 +0100

    feat: add PaymentTranslatorInterface contract
    Create PaymentTranslatorInterface with two methods to switch between the model and the business logic

---
+++ src/QuanticTelecom/InvoicesStorage/Contracts/PaymentTranslatorInterface.php
@@ -0,0 +1,23 @@
+<?php namespace QuanticTelecom\InvoicesStorage\Contracts;
+
+use QuanticTelecom\Invoices\Contracts\PaymentInterface as PaymentLogic;
+use QuanticTelecom\InvoicesStorage\Models\Payment as PaymentModel;
+
+interface PaymentTranslatorInterface
+{
+    /**
+     * @param PaymentModel $paymentModel
+     * @param PaymentLogic $paymentLogic | null Payment logic instance to use
+     * @return PaymentLogic
+     */
+    public function customerModelToPaymentLogic(
+        PaymentModel $paymentModel,
+        PaymentLogic $paymentLogic = null
+    );
+
+    /**
+     * @param PaymentLogic $paymentLogic
+     * @return PaymentModel
+     */
+    public function customerLogicToPaymentModel(PaymentLogic $paymentLogic);
+}
