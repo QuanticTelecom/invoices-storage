2a238d3cafa03a54e42f503f82bbc2cb7e1ca6fc
Thibaud Dauce <thibaud@dauce.fr>
2015-05-15 13:54:23 +0200

    test: add a test for InvoiceArrayValidator

---
+++ tests/InvoiceArrayValidatorTest.php
@@ -0,0 +1,89 @@
+<?php namespace QuanticTelecom\InvoicesStorage\Tests;
+
+use QuanticTelecom\InvoicesStorage\Validators\InvoiceArrayValidator;
+
+class InvoiceArrayValidatorTest extends InvoiceStorageTest {
+
+    /**
+     * @var array
+     */
+    protected $arrayToValidate;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->invoiceArrayValidator = new InvoiceArrayValidator();
+
+        $this->arrayToValidate = [
+            'id' => '42',
+            'customer' => [
+                'type' => 'student'
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function itSucceedsIfAllDataAreProvided()
+    {
+        $data = $this->arrayToValidate;
+
+        $this->assertTrue(
+            $this->invoiceArrayValidator->validate($data)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itFailsIfThereIsNoIdKey()
+    {
+        $data = $this->arrayToValidate;
+        unset($data['id']);
+
+        $this->assertFalse(
+            $this->invoiceArrayValidator->validate($data)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itFailsIfThereIsNoCustomerKey()
+    {
+        $data = $this->arrayToValidate;
+        unset($data['customer']);
+
+        $this->assertFalse(
+            $this->invoiceArrayValidator->validate($data)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itFailsIfCustomerValueIsNotAnArray()
+    {
+        $data = $this->arrayToValidate;
+        $data['customer'] = 'notAnArray';
+
+        $this->assertFalse(
+            $this->invoiceArrayValidator->validate($data)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itFailsIfThereIsNoTypeKeyIntoTheCustomerData()
+    {
+        $data = $this->arrayToValidate;
+        unset($data['customer']['type']);
+
+        $this->assertFalse(
+            $this->invoiceArrayValidator->validate($data)
+        );
+    }
+}
